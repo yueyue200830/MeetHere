41776cc0561f21c5a851c808f97a2a82d60daf54
yueyue200830 <45792349+yueyue200830@users.noreply.github.com>
2019-10-16 23:26:57 +0800

    fix backend bug. delete unused lines.

--- backend/src/main/java/com/proj/meethere/MeethereApplication.java
+++ backend/src/main/java/com/proj/meethere/MeethereApplication.java
@@ -1,9 +1,7 @@
 package com.proj.meethere;
 
-import ch.qos.logback.classic.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MeethereApplication {
