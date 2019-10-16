78afeebb18c9d0459e839dd4923e43bc20c9935a
S_Lei <slei@berkeley.edu>
2019-10-16 10:41:08 +0800

    add backend

---
+++ backend/src/main/java/com/proj/meethere/MeethereApplication.java
@@ -0,0 +1,13 @@
+package com.proj.meethere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeethereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeethereApplication.class, args);
+    }
+
+}
