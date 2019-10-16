17b38704d806adf70048774f4a7e9454b2bb9609
S_Lei <slei@berkeley.edu>
2019-10-16 11:10:21 +0800

    add env

--- backend/src/main/java/com/proj/meethere/MeethereApplication.java
+++ backend/src/main/java/com/proj/meethere/MeethereApplication.java
@@ -1,7 +1,9 @@
 package com.proj.meethere;
 
+import ch.qos.logback.classic.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MeethereApplication {
@@ -9,5 +11,4 @@ public class MeethereApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeethereApplication.class, args);
     }
-
 }
