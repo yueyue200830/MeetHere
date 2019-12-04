71794fd103a4173e0af8c52f42739972ac4e5531
S_Lei <slei@berkeley.edu>
2019-12-04 15:15:03 +0800

    add news get all (user)

---
+++ backend/src/main/java/com/proj/meethere/controller/UserNewsController.java
@@ -0,0 +1,35 @@
+package com.proj.meethere.controller;
+
+import com.proj.meethere.dao.NewsRepository;
+import com.proj.meethere.entity.News;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Tresaresa
+ * @Date 2019-10-29
+ * News Controller
+ */
+@Controller
+@CrossOrigin(origins = "*")
+public class UserNewsController {
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @RequestMapping(value = "/getNewsUserPage", method = RequestMethod.POST)
+    @ResponseBody
+    public String getAllNews() {
+        List<News> newsList = newsRepository.selectAllNews();
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(newsList);
+        return jsonArray.toString();
+    }
+
+}
