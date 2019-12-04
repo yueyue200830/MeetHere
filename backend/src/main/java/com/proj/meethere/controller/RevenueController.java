15911e91a05d1f84f3f90f0427fab05237b745bd
yueyue200830 <yueyue200830@163.com>
2019-12-04 14:25:27 +0800

    Resolve crossorigin

--- backend/src/main/java/com/proj/meethere/controller/RevenueController.java
+++ backend/src/main/java/com/proj/meethere/controller/RevenueController.java
@@ -17,6 +17,7 @@ import java.util.List;
  * Revenue Controller
  */
 @Controller
+@CrossOrigin(origins = "*")
 public class RevenueController {
     @Autowired
     private RevenueRepository revenueRepository;
