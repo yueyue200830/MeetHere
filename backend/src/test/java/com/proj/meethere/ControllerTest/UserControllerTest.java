645756807aebb4a2ebba56df5496664879dc069f
momo <yiqingt7773@gmail.com>
2019-12-18 17:50:52 +0800

    add Test

---
+++ backend/src/test/java/com/proj/meethere/ControllerTest/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.proj.meethere.ControllerTest;
+
+import com.proj.meethere.Service.UserService;
+import com.proj.meethere.controller.UserController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Yiqing Tao
+ * @date 2019-12-18 13:23
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    UserController userController;
+    @Test
+    public void should_get_user() throws Exception{
+        ResultActions perform = mockMvc.perform(get("/getUser"));
+        perform.andExpect(status().isOk());
+        verify(userService,times(1)).getUserInfo();
+    }
+
+    @Test
+    public void should_delete_user() throws Exception{
+        ResultActions perform = mockMvc.perform(get("/deleteUser").param("1"));
+        perform.andExpect(status().isOk());
+        verify(userService, times(1)).deleteSpecificUser(1);
+
+    }
+
+    @Test
+    public void should_select_specific_user() throws Exception {
+        ResultActions perform = mockMvc.perform(get("/searchUser").param("1"));
+        perform.andExpect(status().isOk());
+        verify(userService,times(1)).selectSpecificUserInfo(1);
+    }
+
+}
--- backend/src/test/java/com/proj/meethere/UserServiceTest.java
+++ backend/src/test/java/com/proj/meethere/UserServiceTest.java
@@ -3,12 +3,20 @@ package com.proj.meethere;
 import com.proj.meethere.Service.UserService;
 import com.proj.meethere.dao.UserRepository;
 import com.proj.meethere.entity.User;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author Yiqing Tao
@@ -23,8 +31,40 @@ public class UserServiceTest {
     @MockBean
     private UserRepository userRepository;
 
+    private List<User> userList;
+
+    @Before
+    public void init() {
+        User user1 = new User("Jack","passwordIsSecret",1,"mockUserPhoto");
+        userList = new ArrayList<>();
+        userList.add(user1);
+    }
+    @After
+    public void cleanUp() {
+        this.userList.clear();
+    }
     @Test
     public void get_user_info_should_be_executed() {
-        ]
+        User user2 = new User("test2","passwd",0,"fakePhoto");
+        userList.add(user2);
+        when(userRepository.selectAllUserInfo()).thenReturn(userList);
+        List<User> users = userService.getUserInfo();
+        verify(userRepository,times(1)).selectAllUserInfo();
     }
+
+    @Test
+    public void get_specific_user_should_be_executed() {
+        when(userRepository.selectSpecificUser(1)).thenReturn(userList);
+        List<User> userResult = userService.selectSpecificUserInfo(1);
+        verify(userRepository,times(1)).selectSpecificUser(1);
+    }
+
+    @Test
+    public void delete_specific_user_should_be_executed() {
+        when(userRepository.deleteSpecificUser(1)).thenReturn(1);
+        int result = userService.deleteSpecificUser(1);
+        verify(userRepository,times(1)).deleteSpecificUser(1);
+        Assert.assertEquals(1,result);
+    }
+
 }
