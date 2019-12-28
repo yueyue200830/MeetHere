99e6f8042ce167caca89531fc510cca41aa4ee39
momo7773 <47842212+momo7773@users.noreply.github.com>
2019-12-28 22:43:29 +0800

    update name and complete 2/5 normal interface test

--- backend/src/test/java/com/proj/meethere/interfaceTest/messageInterfaceTest.java
+++ backend/src/test/java/com/proj/meethere/interfaceTest/messageInterfaceTest.java
@@ -1,17 +1,30 @@
 package com.proj.meethere.interfaceTest;
 
+import com.proj.meethere.utils.TestUtils;
+import com.sun.jndi.toolkit.url.Uri;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Yiqing Tao
@@ -25,17 +38,53 @@ public class messageInterfaceTest {
         //URI uri = new URIBuilder().setScheme("http").setHost("localhost:8080").setPath("/searchUnapproveMessage/1").build();
         HttpPost httpPost = new HttpPost("http://localhost:8081/getUnapproveMessage");
         HttpResponse response = httpClient.execute(httpPost);
-        HttpEntity entity = response.getEntity();
-        Assert.assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode());
-        System.out.println(entity.getContent());
-        httpClient.getConnectionManager().shutdown();
-
+        InputStream inputStream = response.getEntity().getContent();
+        JSONArray jsonArray = TestUtils.inputStream2JSONArray(inputStream);
+        System.out.println(jsonArray);
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        assertAll(()->assertEquals(HttpStatus.SC_OK, response.getStatusLine().getStatusCode()),
+                ()->assertTrue(jsonObject.keySet().contains("messageTime")),
+                ()->assertTrue(jsonObject.keySet().contains("messageTitle")),
+                ()->assertTrue(jsonObject.keySet().contains("messageVisibility")),
+                ()->assertTrue(jsonObject.keySet().contains("userId")),
+                ()->assertTrue(jsonObject.keySet().contains("messageContent")),
+                ()->assertTrue(jsonObject.keySet().contains("id")));
+        httpClient.close();
     }
 
-    @AfterClass
-    public static void afterClass() {
-        //断开连接
-
+    @Test
+    public void should_search_unapproved_message() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString("http://localhost:8081/searchUnapproveMessage/{condition}").build();
+        URI uri = uriComponents.expand("1").encode().toUri();
+        HttpGet httpGet = new HttpGet(uri);
+        HttpResponse httpResponse = httpClient.execute(httpGet);
+        InputStream inputStream = httpResponse.getEntity().getContent();
+        JSONArray jsonArray = TestUtils.inputStream2JSONArray(inputStream);
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        assertAll(()->assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode()),
+                ()->assertTrue(jsonObject.keySet().contains("messageTime")),
+                ()->assertTrue(jsonObject.keySet().contains("messageTitle")),
+                ()->assertTrue(jsonObject.keySet().contains("messageVisibility")),
+                ()->assertTrue(jsonObject.keySet().contains("userId")),
+                ()->assertTrue(jsonObject.keySet().contains("messageContent")),
+                ()->assertTrue(jsonObject.keySet().contains("id")));
+        httpClient.close();
     }
+
+    @Test
+    public void should_approve_message() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString("http://localhost:8081/approveMessage/{temp}").build();
+        URI uri = uriComponents.expand("1").encode().toUri();
+        HttpGet httpGet = new HttpGet(uri);
+        HttpResponse httpResponse = httpClient.execute(httpGet);
+        InputStream inputStream = httpResponse.getEntity().getContent();
+        int result = Integer.parseInt(TestUtils.inputStream2String(inputStream));
+        assertAll(()->assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode()),
+                ()->assertTrue(result == 1 || result == 0));
+    }
+
+
 
 }
