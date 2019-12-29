b53387940fa098a5ea0a9a9399e3439956e0a880
yueyue200830 <45792349+yueyue200830@users.noreply.github.com>
2019-12-29 18:25:55 +0800

    Use server backend and database

--- fronend2.0/vue.config.js
+++ fronend2.0/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/app': {
-        target: 'http://localhost:8081/',
+        target: 'http://49.235.124.124/:8081/',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
