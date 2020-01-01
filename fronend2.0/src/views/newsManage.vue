199307c004656efcbefaa8a3e146d2fd5286da38
ballballtang <ttangwei331998@163.com>
2020-01-01 14:17:50 +0800

    add an upload picture tip

--- fronend2.0/src/views/newsManage.vue
+++ fronend2.0/src/views/newsManage.vue
@@ -51,7 +51,7 @@
                   :limit="1"
                   list-type="picture">
                   <el-button size="small" type="primary">点击上传</el-button>
-                  <!-- <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div> -->
+                  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过1M</div>
                 </el-upload>
               </el-form-item>
 
