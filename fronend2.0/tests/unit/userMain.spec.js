8e0755bb21c4933267fae4c793b58309657b9030
yueyue200830 <45792349+yueyue200830@users.noreply.github.com>
2020-01-01 21:49:18 +0800

    add a simple unit test for user main page.

---
+++ fronend2.0/tests/unit/userMain.spec.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import UserMain from '@/views/userMain.vue'
+import MockAdapter from "axios-mock-adapter"
+import flushPromises from "flush-promises"
+import ElementUI from 'element-ui'
+import axios from 'axios'
+
+const localVue = createLocalVue();
+localVue.use(ElementUI);
+localVue.prototype.$http = axios;
+
+const mock = new MockAdapter(localVue.prototype.$http);
+
+describe('userMain.vue', () => {
+
+  it('renders props.msg when passed', async () => {
+    let revenue = ['篮球馆', '足球场']
+    let newsList = [{
+      newsPhoto: 'test photo',
+      newsTitle: 'test title',
+      newsContent: 'test content'
+    }]
+    mock
+      .onPost('/app/getVenueName')
+      .reply(200, [revenue])
+    mock
+      .onPost('/app/getNewsThree')
+      .reply(200, newsList)
+    const wrapper = shallowMount(UserMain, {localVue})
+    await flushPromises();
+    expect(wrapper.vm.revenue).to.eql(revenue)
+    expect(wrapper.vm.newsList).to.eql(newsList)
+  })
+})
