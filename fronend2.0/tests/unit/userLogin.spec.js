9a7af5880030c36d01e707c903f0fe0b9282f74a
yueyue200830 <yueyue200830@163.com>
2020-01-02 19:59:18 +0800

    Add unit test for front end.

--- fronend2.0/tests/unit/userLogin.spec.js
+++ fronend2.0/tests/unit/userLogin.spec.js
@@ -3,6 +3,7 @@ import {createLocalVue, mount} from '@vue/test-utils'
 import UserLogin from '@/views/userLogin.vue'
 import MockAdapter from "axios-mock-adapter"
 import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
 import axios from 'axios'
 import Vuex from 'vuex'
 import sinon from 'sinon'
@@ -11,6 +12,8 @@ const localVue = createLocalVue()
 localVue.use(ElementUI)
 localVue.use(Vuex)
 localVue.prototype.$http = axios
+localVue.use(VueRouter)
+const router = new VueRouter()
 
 const mock = new MockAdapter(localVue.prototype.$http);
 
@@ -31,6 +34,32 @@ describe('userLogin.vue', () => {
     mock
       .onGet('/app/Login', {params: {login: loginForm}})
       .reply(200, 1)
+    const wrapper = mount(UserLogin, {localVue, store, router})
+
+    wrapper.find('input[type=name]').setValue(loginForm.name)
+    wrapper.find('input[type=password]').setValue(loginForm.password)
+
+    expect(wrapper.vm.loginForm).to.eql(loginForm)
+    wrapper.vm.login()
+    expect(mutations.userLogin.calledOnce);
+  })
+
+  it('wrong password', () => {
+    let mutations = {
+      userLogin: sinon.fake()
+    }
+    let store = new Vuex.Store({
+      state: {},
+      mutations
+    })
+    let loginForm = {
+      name: 'testUser',
+      password: '123456zA-'
+    }
+
+    mock
+      .onGet('/app/Login', {params: {login: loginForm}})
+      .reply(200, -1)
     const wrapper = mount(UserLogin, {localVue, store})
 
     wrapper.find('input[type=name]').setValue(loginForm.name)
@@ -38,5 +67,6 @@ describe('userLogin.vue', () => {
 
     expect(wrapper.vm.loginForm).to.eql(loginForm)
     wrapper.vm.login()
+    sinon.assert.notCalled(mutations.userLogin)
   })
 })
